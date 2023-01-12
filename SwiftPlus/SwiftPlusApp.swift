af8a0f0ae81806c5edf5368f21c41b350137301d
suyashlunawat <92525202+suyashlunawat@users.noreply.github.com>
2023-01-12 14:51:34 +0100

    Initial Commit

---
+++ SwiftPlus/SwiftPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftPlusApp.swift
+//  SwiftPlus
+//
+//  Created by Suyash Lunawat on 12/01/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
