edc68edddb572e121de5422c81f666174844dc40
Leila Najar <lnajar22@fed.idserve.net>
2023-01-12 17:53:32 +0100

    make mainScreenView and tabBarview

---
+++ SwiftPlus/contactView.swift
@@ -0,0 +1,20 @@
+//
+//  contactView.swift
+//  SwiftPlus
+//
+//  Created by leila najar on 12/01/23.
+//
+
+import SwiftUI
+
+struct contactView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct contactView_Previews: PreviewProvider {
+    static var previews: some View {
+        contactView()
+    }
+}
